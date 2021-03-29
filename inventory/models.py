012cb45f564f202018170fed382abf3f9522bdd3
Yusuf Sabuwala <yes5@njit.edu>
2021-03-28 21:40:27 -0400

    added color option

--- inventory/models.py
+++ inventory/models.py
@@ -4,19 +4,29 @@ from django.forms import ModelForm
 # Create your models here.
 
 class Shirt(models.Model):
-    sale_date = models.DateField()
+    shirt_color = models.CharField(
+        max_length = 10,
+        choices=[
+            ('White',"White"),
+            ('Black',"Black"),
+        ]
+    )
     shirt_size = models.CharField(
-        max_length = 2,
+        max_length = 3,
         choices=[
             ('S',"Small"),
             ('M',"Medium"),
             ('L',"Large"),
-            ('XL',"Extra Large")
+            ('XL',"Extra Large"),
         ]
     )
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField()
+    sale_date = models.DateField(auto_now=True)
+    
+    #class Meta:
+        #sequence = ('id','shirt_color','shirt_size','quantity','sale_date')
 
 class ShirtForm(ModelForm):
     class Meta:
         model = Shirt
-        fields = ['sale_date','shirt_size', 'quantity']
+        fields = ['shirt_color','shirt_size', 'quantity']
